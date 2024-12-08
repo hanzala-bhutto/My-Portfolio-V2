c6a27544d0bddf9cf851ae5cb7bf9db47c1fab12
hanzala-bhutto <bhutto.hanzala@gmail.com>
2024-12-08 15:30:09 +0100

    Added Xing Profile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Footer.scss'
+import { FaGithub, FaLinkedin, FaXingSquare } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/hanzala-bhutto" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/hanzala-bhutto-a10936234/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/hanzala-bhutto" target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/hanzala-bhutto-a10936234/" target="_blank" rel="noreferrer"><FaLinkedin/></a>
+        <a href="https://www.xing.com/profile/Hanzala_bhutto" target="_blank" rel="noreferrer"><FaXingSquare /></a>
       </div>
     </footer>
   );
